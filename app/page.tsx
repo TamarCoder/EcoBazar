ba8f14524916b82f2e07099265f9d86750a7aa2e
Tamar khuskivadze <thamuna.khuskivadze@gmail.com>
2025-10-16 18:41:19 +0400

    addd

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
-import Button from "./CustomButton/CustomBUtton";
+
  
 export default function MainPage() {
   return (
-    <Button
-      title="Search"
-      variant="PRIMARY"
-      size="sm"
-      type="BUTTON"
-      className="primary"
-    />
+    
   );
 }
